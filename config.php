c4ce2866ab921739e5ad4cdc3012038f0cbbb2ea
Iulia Sincai <isincai@pitechnologies.ro>
2019-04-08 21:36:00 +0300

    Add db config

--- config.php
+++ config.php
@@ -1,7 +1,25 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db_name = substr($url["path"], 1);
+  $db = new mysqli($server, $username, $password, $db_name);
+?>
+
+<!-- <?php
+   define('DB_SERVER', 'mysql://bc02618683702b:b5a58968@eu-cdbr-west-02.cleardb.net/heroku_c25aa04300f0c23?reconnect=true');
+   define('DB_USERNAME', 'bc02618683702b');
+   define('DB_PASSWORD', 'b5a58968');
+   define('DB_DATABASE', 'heroku_c25aa04300f0c23');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?> -->
+
+<!-- <?php
    define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'art_attack_db');
+   define('DB_DATABASE', 'art-attack-db');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
+?> -->
