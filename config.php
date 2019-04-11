81000aef366c171ee884057ffeef29f591e28142
Iulia Sincai <isincai@pitechnologies.ro>
2019-04-11 09:33:35 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
@@ -6,15 +6,15 @@ $username = $url["user"];
 $password = $url["pass"];
 $db_name = substr($url["path"], 1);
   $db = new mysqli($server, $username, $password, $db_name);
-?>
+?> -->
 
-<!-- <?php
+<?php
    define('DB_SERVER', 'mysql://bc02618683702b:b5a58968@eu-cdbr-west-02.cleardb.net/heroku_c25aa04300f0c23?reconnect=true');
    define('DB_USERNAME', 'bc02618683702b');
    define('DB_PASSWORD', 'b5a58968');
    define('DB_DATABASE', 'heroku_c25aa04300f0c23');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?> -->
+?>
 
 <!-- <?php
    define('DB_SERVER', 'localhost:3306');
