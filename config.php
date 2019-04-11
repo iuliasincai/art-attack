886888fbe967d8171146c54ed7bbdcff26db8741
Iulia Sincai <isincai@pitechnologies.ro>
2019-04-11 09:40:29 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,35 @@
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'    => '',
+    'hostname' => $cleardb_server,
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
 <!-- <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
@@ -8,13 +40,13 @@ $db_name = substr($url["path"], 1);
   $db = new mysqli($server, $username, $password, $db_name);
 ?> -->
 
-<?php
+<!-- <?php
    define('DB_SERVER', 'mysql://bc02618683702b:b5a58968@eu-cdbr-west-02.cleardb.net/heroku_c25aa04300f0c23?reconnect=true');
    define('DB_USERNAME', 'bc02618683702b');
    define('DB_PASSWORD', 'b5a58968');
    define('DB_DATABASE', 'heroku_c25aa04300f0c23');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
+?> -->
 
 <!-- <?php
    define('DB_SERVER', 'localhost:3306');
