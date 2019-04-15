dfca65b641fb4185e9de3ea9f1d8bbeefe3d7971
Iulia Sincai <isincai@pitechnologies.ro>
2019-04-15 12:48:48 +0300

    Update db config

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'art_attack_db');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+//   define('DB_SERVER', 'localhost:3306');
+//   define('DB_USERNAME', 'root');
+//   define('DB_PASSWORD', '');
+//   define('DB_DATABASE', 'art_attack_db');
+//   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+//?>
+<?php
+define('DB_SERVER', 'eu-cdbr-west-02.cleardb.net');
+define('DB_USERNAME', 'bc02618683702b');
+define('DB_PASSWORD', 'b5a58968');
+define('DB_DATABASE', 'heroku_c25aa04300f0c23');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
